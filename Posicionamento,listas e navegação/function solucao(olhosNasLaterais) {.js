cf52b0a698e2cae541b5ae11e531db0907bcd3db
Samantha Alvarenga <samanthaalvarengaa.l23@gmail.com>
2023-06-25 00:31:14 -0300

    adicionado arquivo de texto

---
+++ Posicionamento,listas e navegação/function solucao(olhosNasLaterais) {.js
@@ -0,0 +1,10 @@
+function solucao(olhosNasLaterais) {
+   
+    const animal =  olhosNasLaterais ;
+ 
+    if(olhosNasLaterais == presa){
+        console.log(" presa");
+    }else {
+        console.log(" predador");    
+    }
+ }
